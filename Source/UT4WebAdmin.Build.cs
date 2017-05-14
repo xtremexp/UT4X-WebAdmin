40133397ab57934e072a11bdb6fa15fa402ec995
xtremexp <unrealxtx@gmail.com>
2017-05-14 14:49:26 +0200

    useless

--- Source/UT4WebAdmin.Build.cs
+++ Source/UT4WebAdmin.Build.cs
@@ -5,7 +5,6 @@ namespace UnrealBuildTool.Rules
 	{
 		public UT4WebAdmin(TargetInfo Target)
         {
-            //PrivateIncludePaths.Add("UT4WebAdmin/Private");
             PublicDependencyModuleNames.AddRange(
 				new string[]
 				{
