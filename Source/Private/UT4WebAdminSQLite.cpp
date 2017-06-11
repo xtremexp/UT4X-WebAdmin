cf28a0e4c873c4aa965364868a89e5038fad9d42
xtremexp <unrealxtx@gmail.com>
2017-06-11 09:57:50 +0200

    added human readable SQLite result code function nearly done with binding fstring parameters

--- Source/Private/UT4WebAdminSQLite.cpp
+++ Source/Private/UT4WebAdminSQLite.cpp
@@ -7,6 +7,46 @@ UUT4WebAdminSQLite::UUT4WebAdminSQLite(const FObjectInitializer& ObjectInitializ
 	SetFlags(RF_MarkAsRootSet);
 }
 
+/* Get human comprehensive sqlite result code meaning.*/
+FString ResultCodeToMessage(int resultCode) {
+	FString message = "";
+
+	switch (resultCode) {
+		case SQLITE_ERROR: message = "SQL error or missing database"; break;
+		case SQLITE_INTERNAL: message = "Internal logic error in SQLite"; break;
+		case SQLITE_PERM: message = "Access permission denied"; break;
+		case SQLITE_ABORT: message = "Callback routine requested an abort"; break;
+		case SQLITE_BUSY: message = "The database file is locked"; break;
+		case SQLITE_LOCKED: message = "A table in the database is locked"; break;
+		case SQLITE_NOMEM: message = "A malloc() failed"; break;
+		case SQLITE_READONLY: message = "Attempt to write a readonly database"; break;
+		case SQLITE_INTERRUPT: message = " Operation terminated by sqlite3_interrupt()"; break;
+		case SQLITE_NOTFOUND: message = "Unknown opcode in sqlite3_file_control()"; break;
+		case SQLITE_FULL: message = "Insertion failed because database is full"; break;
+		case SQLITE_CANTOPEN: message = "Unable to open the database file"; break;
+		case SQLITE_PROTOCOL: message = "Database lock protocol error"; break;
+		case SQLITE_EMPTY: message = "Database is empty"; break;
+		case SQLITE_SCHEMA: message = "The database schema changed"; break;
+		case SQLITE_TOOBIG: message = " String or BLOB exceeds size limit"; break;
+		case SQLITE_CONSTRAINT: message = "Abort due to constraint violation"; break;
+		case SQLITE_MISMATCH: message = "Data type mismatch"; break;
+		case SQLITE_MISUSE: message = "Library used incorrectly"; break;
+		case SQLITE_NOLFS: message = "Uses OS features not supported on host"; break;
+		case SQLITE_AUTH: message = "Authorization denied"; break;
+		case SQLITE_FORMAT: message = "Auxiliary database format error"; break;
+		case SQLITE_RANGE: message = " 2nd parameter to sqlite3_bind out of range"; break;
+		case SQLITE_NOTADB: message = "File opened that is not a database file"; break;
+		case SQLITE_NOTICE: message = "Notifications from sqlite3_log()"; break;
+		case SQLITE_WARNING: message = "Warnings from sqlite3_log()"; break;
+		case SQLITE_ROW: message = "sqlite3_step() has another row ready"; break;
+		case SQLITE_DONE: message = "sqlite3_step() has finished executing "; break;
+		default:
+		break;
+	}
+
+	return message;
+};
+
 void UUT4WebAdminSQLite::Start() {
 	// TODO one db file per instance server ?
 	// TODO create file if not exists
@@ -21,7 +61,7 @@ void UUT4WebAdminSQLite::Start() {
 		}
 		else
 		{
-			UE_LOG(UT4WebAdmin, Warning, TEXT("Failed at opening SQLite DB file %s with SQLite. Error code: %i"), *DatabasePath, resultCode);
+			UE_LOG(UT4WebAdmin, Warning, TEXT("Failed at opening SQLite DB file %s with SQLite. Error code: %i %s"), *DatabasePath, resultCode, *ResultCodeToMessage(resultCode));
 			UE_LOG(UT4WebAdmin, Warning, TEXT("Some features such as chat logs might not be available."));
 		}
 	}
@@ -34,7 +74,7 @@ void UUT4WebAdminSQLite::Stop() {
 
 		// result code might be SQLITE_BUSY
 		if (resultCode != SQLITE_OK) {
-			UE_LOG(UT4WebAdmin, Warning, TEXT("Could not close SQLite DB connection. Error code: %i"), resultCode);
+			UE_LOG(UT4WebAdmin, Warning, TEXT("Could not close SQLite DB connection. Error code: %i %s"), resultCode, *ResultCodeToMessage(resultCode));
 		}
 	}
 }
@@ -100,62 +140,54 @@ bool UUT4WebAdminSQLite::ExecDatabaseCommand(const FString& DatabaseCommand, TAr
 void UUT4WebAdminSQLite::SaveChatMessage(const FString& senderName, const FUniqueNetIdRepl& senderUniqueId, int32 senderTeamNum, const FString& message) {
 
 	if ( Database ) {
-
 		FString sql = "INSERT INTO `ut4webadmin_chat` (`time`, `sender_name`, `sender_uid`, `sender_team_num`, `message`) VALUES (?, ?, ?, ?, ?);";
 
 		sqlite3_stmt* stmt = nullptr;
 		int resultCode = sqlite3_prepare_v2(Database, TCHAR_TO_ANSI(*sql), -1, &stmt, nullptr);
 
 		if (resultCode == SQLITE_OK) {
-			FString now = FDateTime::Now().ToIso8601();
-			FString uidStr = senderUniqueId.ToString();
 
-			//bind_text(stmt, 1, now);
-			//const char* nowChar = (const char *)TCHAR_TO_UTF8(*now);
-
-			bind_text(stmt, 1, now);
-			bind_text(stmt, 2, uidStr);
-			bind_text(stmt, 3, senderName);
-			//sqlite3_bind_text(stmt, 1, nowChar, strlen(nowChar), SQLITE_STATIC);
-			//sqlite3_bind_text(stmt, 2, (const char *)TCHAR_TO_UTF8(*uidStr), uidStr.Len(), SQLITE_STATIC);
-			//sqlite3_bind_text(stmt, 3, (const char *)TCHAR_TO_UTF8(*senderName), senderName.Len(), SQLITE_STATIC);
+			bind_text(stmt, 1, FDateTime::Now().ToIso8601());
+			bind_text(stmt, 2, senderName);
+			bind_text(stmt, 3, senderUniqueId.ToString());
 			sqlite3_bind_int(stmt, 4, senderTeamNum);
 			bind_text(stmt, 5, message);
-			//sqlite3_bind_text(stmt, 5, (const char *)TCHAR_TO_UTF8(*message), message.Len(), SQLITE_STATIC);
 
 			resultCode = sqlite3_step(stmt);
 			if (resultCode != SQLITE_DONE) {
-				UE_LOG(UT4WebAdmin, Warning, TEXT("Error inserting data. SQL: %s. Result code: %i"), *sql, resultCode);
+				UE_LOG(UT4WebAdmin, Warning, TEXT("Error inserting data. SQL: %s. Result code: %i %s"), *sql, resultCode, *ResultCodeToMessage(resultCode));
 			}
 
 			resultCode = sqlite3_finalize(stmt);
 		}
-		// at this stage table might be missing
 		else {
-			UE_LOG(UT4WebAdmin, Warning, TEXT("SQLite prepare failed. SQL: %s. Result code: %i"), *sql, resultCode);
+			UE_LOG(UT4WebAdmin, Warning, TEXT("SQLite prepare failed. SQL: %s. Result code: %i %s"), *sql, resultCode, *ResultCodeToMessage(resultCode));
 		}
 	}
 }
 
-// FIXME seems to always bind pointer adress
+// bind FString parameter for sqlite statement
+// note: by default db encoding is UTF-8 in SQLite
 int UUT4WebAdminSQLite::bind_text(sqlite3_stmt* stmt, int idx, const FString& text) {
 
 	int resultCode = SQLITE_ERROR;
 
 	if (*text != NULL) {
-		UE_LOG(UT4WebAdmin, Warning, TEXT("Text: %s"), *text);
-
-		resultCode = sqlite3_bind_text(stmt, idx, (const char *) text.GetCharArray().GetData(), text.Len(), SQLITE_STATIC);
+		// tricky way to make it work ... TODO better way ?
+		char* utf8Char = new char[text.GetAllocatedSize()];
+		FTCHARToUTF8_Convert::Convert(utf8Char, sizeof(utf8Char), text.GetCharArray().GetData(), sizeof(text.GetCharArray().GetData()));
+		
+		resultCode = sqlite3_bind_text(stmt, idx, utf8Char, sizeof(utf8Char), SQLITE_STATIC);
 
 		if (resultCode != SQLITE_OK) {
-			UE_LOG(UT4WebAdmin, Warning, TEXT("Could not bind text '%s' parameter for sql query at index %i. Result code: %i"), *text, idx, resultCode);
+			UE_LOG(UT4WebAdmin, Warning, TEXT("Could not bind text '%s' parameter for sql query at index %i. Result code: %i %s"), *text, idx, resultCode, *ResultCodeToMessage(resultCode));
 		}
 	}
 	else {
 		resultCode = sqlite3_bind_null(stmt, idx);
 
 		if (resultCode != SQLITE_OK) {
-			UE_LOG(UT4WebAdmin, Warning, TEXT("Could not bind text null parameter for sql query at index %i. Result code: %i"), idx, resultCode);
+			UE_LOG(UT4WebAdmin, Warning, TEXT("Could not bind text null parameter for sql query at index %i. Result code: %i %s"), idx, resultCode, *ResultCodeToMessage(resultCode));
 		}
 	}
 
